1737b40be27ee37022c6e5dd59e7730caf353414
Robert Shirak <bcshirak1@gmail.com>
2020-10-14 12:52:52 -0400

    html content and java variables no css or java logic

---
+++ script.js
@@ -0,0 +1,20 @@
+let questions = document.querySelectorAll('.question-content')
+
+let directions= document.querySelector('.directions')
+
+let startButton= document.querySelector('.start-button')
+
+let options = document.querySelectorAll('.options')
+
+let correct =document.querySelectorAll('.correct-answer')
+
+let results = document.querySelector('.results')
+
+let restartGame = document.querySelector('.restart-game')
+
+// possible logic: console.log the question answers and use classList
+// classList to keep track of correct/correct-answer that are 
+// console logged.. use that to also keep track of score
+
+//once finished creating entire project try to figure out how to show 
+// a progress bar at the bottom of the answer choices
